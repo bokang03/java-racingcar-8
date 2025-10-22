cbeed019d9e62fb370277015b72f175b4583c52a
bogan <bogang2000@naver.com>
2025-10-22 22:06:28 +0900

    fix : CarName 유효성 검사 수정

--- src/main/java/racingcar/CarName.java
+++ src/main/java/racingcar/CarName.java
@@ -5,12 +5,20 @@ public class CarName {
     private String name;
 
     public CarName(String name) {
+        this.name = name;
+        validateNameBlank(name);
+        validateNameLength(name);
+    }
+
+    public void validateNameBlank(String name){
         if (name == null||name.isBlank()) {
             throw new IllegalArgumentException("자동차 이름을 입력해주세요.");
         }
+    }
+
+    public void validateNameLength(String name) {
         if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("자동차 이름은 " + MAX_NAME_LENGTH +"자 이하만 가능합니다.");
         }
-        this.name = name;
     }
 }
