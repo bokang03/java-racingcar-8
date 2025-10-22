38b86f97a4d7fbdd99bbbc123c482b1e078ce363
bogan <bogang2000@naver.com>
2025-10-22 23:01:06 +0900

    test : 정상 입력 및 5자 초과 경우와 공백 입력 예외 테스트 추가

---
+++ src/test/java/racingcar/CarNameTest.java
@@ -0,0 +1,22 @@
+package racingcar;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class CarNameTest {
+
+    @Test
+    void 이름_공백_검증() {
+        List<String> names = List.of("pobi", "bokang", " ");
+
+        for (String name : names) {
+            try {
+                CarName carName = new CarName(name);
+                System.out.println(name + " : 성공");
+            } catch (IllegalArgumentException e) {
+                System.out.println(name +" : " + e.getMessage());
+            }
+        }
+    }
+}
